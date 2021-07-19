de3f80b47e8b4fa2e680b5b3e3d1a65169ac701a
M. Montesano <55593359+mmontesanonyc@users.noreply.github.com>
2021-07-19 14:40:28 -0400

    Adding housing files

---
+++ housing/map/mapcd.js
@@ -0,0 +1,46 @@
+
+let cdSpec = "mapcd.vl.json";
+let pumaSpec = "mappuma.vl.json";
+let ntaSpec = "mapnta.vl.json";
+
+const opt = {"renderer":"svg"};
+const el = document.getElementById('map');
+
+
+// this code listens to the form with map chooser; must run after DOM loads
+window.onload =listenRadios;
+
+function listenRadios() {
+  radios = document.querySelectorAll('input[type=radio][name="mainRadioGroup"]');
+  radios.forEach(radio => radio.addEventListener('change', () => {
+    if (radio.value==='CD') {cdMapCreate()}
+    else if (radio.value==='nta') {ntaMapCreate()}
+    else {pumaMapCreate()}  // for if chosenField is PUMA
+    ;
+  }));
+}
+
+
+//this function creates the map
+function cdMapCreate() {
+vegaEmbed('#map', cdSpec, opt).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    viewObj = result.view;
+  }).catch(console.error);
+}
+
+function pumaMapCreate() {
+  vegaEmbed('#map', pumaSpec, opt).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+      viewObj = result.view;
+    }).catch(console.error);
+  }
+
+  function ntaMapCreate() {
+    vegaEmbed('#map', ntaSpec, opt).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        viewObj = result.view;
+      }).catch(console.error);
+    }
+
+pumaMapCreate();
